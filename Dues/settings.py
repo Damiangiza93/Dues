67dd0bc5b8a0fee7b036a24c3a72904dc2e5cd28
Damian Giza <damian.giza1@gmail.com>
2021-04-12 12:33:36 +0200

    static root correct

--- Dues/settings.py
+++ Dues/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIV_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
